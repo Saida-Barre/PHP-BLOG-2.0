00dcdf6c4ef0782731b5eab03f2a934af5d9e8a0
unknown <sbarre33@gmail.com>
2020-08-15 17:13:31 -0600

    outputed in class

--- includes/articles-api.php
+++ includes/articles-api.php
@@ -1,7 +1,20 @@
 <?php
 function retrieveArticles(){
     $responseString = file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/data/articles.json'); //added a root path due to differences of file types.
-var_dump($responseString);
+//var_dump($responseString); - tested and recieved a response
 
+if($responseString !==FALSE){
+    if(($responseObj = json_decode($responseString))!==NULL){
+    //var_dump($responseObj); - tested and shows the decoding 
+    $results = $responseObj->articles;
+    //var_dump($results);//now test has passed we can now access all articles in the aray
+    } else {
+        echo 'could not interprest API reponse';
+    } 
+    }else {
+        echo 'unable tp connect/retrieve data from API.';
+
+    }
 }
-retrieveArticles();
+
+
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+include 'includes/articles-api.php'; //import stuff from our api file
 include 'includes/Articles.Class.php'; //import stuff from Articles class
 global $title;
 
@@ -9,46 +10,7 @@ include 'templates/header.php'; //imports stuff from header
 
 <h1><?php echo $title; ?></h1> <!---this just outputs the title-->
 
-<h2> Article Test Title </h2>
-
-<?php //adding new object for the class Article.
-   //submit the new ID, title, and blog content through article
-   $blog1 = new Articles();
-   $blog1->blogTitle = 'testing title 1';
-   echo $blog1->blogTitle;
-   $blog1->blogContent = 'testing content 1';
-   echo $blog1->blogContent;
-   ?>
-
-   <ul>
-    <?php
-        $myArticles = array(
-            ( new Articles( 1, 'title1', 'content1')),
-            ( new Articles( 2, 'title2', 'content2')),
-            ( new Articles( 3, 'title3', 'content3')),
-            ( new Articles( 4, 'title4', 'content4')),
-            ( new Articles( 5, 'title5', 'content5')),
-            ( new Articles( 6, 'title6', 'content6'))
-        );
-        foreach ($myArticles as $article):
-            ?>
-            <h3><?php echo $article->blogTitle; ?></h3>
-            <h4><?php echo $article->id; ?></h4>
-            <p>
-                <?php
-                echo $article->blogContent;
-                ?>
-            </p>
-            <?php
-            endforeach;
-            ?>
-
-   </ul>
-
-
-
-
-
+<h2> Our Articles!! Yaaaah! </h2>
 
 
 
