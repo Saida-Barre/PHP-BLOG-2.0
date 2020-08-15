775c222439755d67221ee78308dc662ce5d2a1f9
unknown <sbarre33@gmail.com>
2020-08-15 15:21:32 -0600

    first commit

---
+++ includes/Articles.Class.php
@@ -0,0 +1,8 @@
+<?php
+
+class Articles
+{
+    public $id = 0;
+    public $title= "";
+    public $content="";
+}
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+global $title;
+
+$title = 'PHP Blog Homepage';
+
+include 'templates/header.php';
+?>
+
+<h1><?php echo $title; ?></h1>
+
+<?php include 'templates/footer.php';
